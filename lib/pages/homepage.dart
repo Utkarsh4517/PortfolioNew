eaccef22f2647741977547b981c38b35c61fdbde
Utkarsh Shrivastava <uttkarsh.dav@gmail.com>
2023-04-21 16:46:29 +0530

    created custom text button widget

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:newportfolio/widgets/textbutton.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -11,37 +13,47 @@ class HomePage extends StatelessWidget {
         child: Align(
           alignment: const AlignmentDirectional(0, 0),
           child: Container(
-            width: 1200,
-            decoration: const BoxDecoration(
-              color: Colors.white
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                const SizedBox(height: 10,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      
-                      children:  [
-                        const Icon(Icons.circle, color: Colors.yellowAccent,),
-                        const SizedBox(width: 7,),
-                        Text('UTKARSH', style: GoogleFonts.chivo(color: Colors.black, fontWeight: FontWeight.bold),)
-                      ],
-                    ), 
-                    Row(
-                     
-                      children: [
-                        TextButton(onPressed: (){}, child: Text('Home', style: GoogleFonts.chivo(color: Colors.black, fontWeight: FontWeight.bold),))
-                      ],
-                    )
-                  ],
-                )
-              ],
-
-            )
-          ),
+              width: 1200,
+              decoration: const BoxDecoration(color: Colors.white),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.circle,
+                            color: Colors.yellowAccent,
+                          ),
+                          const SizedBox(
+                            width: 7,
+                          ),
+                          Text(
+                            'UTKARSH',
+                            style: GoogleFonts.chivo(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: const [
+                          CustomTextButton(text: 'Home'),
+                          SizedBox(width: 30,),
+                          CustomTextButton(text: 'Projects',),
+                          SizedBox(width: 30,),
+                          CustomTextButton(text: 'Contact',)
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              )),
         ),
       ),
     );
---
+++ lib/widgets/textbutton.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomTextButton extends StatelessWidget {
+  final String text;
+  const CustomTextButton({required this.text, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {},
+      child: Text(
+        text,
+        style: GoogleFonts.chivo(
+            color: Colors.black, fontWeight: FontWeight.w600, fontSize: 15),
+      ),
+    );
+  }
+}
