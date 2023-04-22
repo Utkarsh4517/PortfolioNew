d019d6cd2ca742853dc0ea887e93bff0e80c3417
Utkarsh Shrivastava <uttkarsh.dav@gmail.com>
2023-04-23 02:01:26 +0530

    added content to introContainer

--- lib/widgets/introcontainer.dart
+++ lib/widgets/introcontainer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class IntroContainer extends StatelessWidget {
   const IntroContainer({super.key});
@@ -19,6 +20,42 @@ class IntroContainer extends StatelessWidget {
                     end: Alignment.bottomLeft,
                   ),
                   borderRadius: BorderRadius.circular(20)),
+              child: Column(
+                children: [
+                  const SizedBox(height: 50,),
+                  Align(
+                    alignment: const AlignmentDirectional(-1, 0),
+                    child: Container(
+                      width: 600,
+                      padding: const EdgeInsets.only(left: 40, right: 10),
+                      child: Text(
+                        "Hello, I'm Utkarsh, a passionate Flutter developer and an open-source enthusiast",
+                        style: GoogleFonts.roboto(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 35,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 60,),
+                  Align(
+                    alignment: const AlignmentDirectional(-1, 0),
+                    child: Container(
+                      width: 700,
+                      padding: const EdgeInsets.only(left: 40, right: 10),
+                      child: Text(
+                        "I care a lot about using design for positive impact. and enjoy creating user-centric, delightful, and human experiences.",
+                        style: GoogleFonts.roboto(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w300,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           )
         : Container(
