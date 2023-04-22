665cee464422cb760d1cc51c0aa33d8761198f0a
Utkarsh Shrivastava <uttkarsh.dav@gmail.com>
2023-04-23 01:32:48 +0530

    added smooth zoom effect in project

--- lib/widgets/projectcontainer.dart
+++ lib/widgets/projectcontainer.dart
@@ -10,36 +10,46 @@ class ProjectContainer extends StatefulWidget {
 }
 
 class _ProjectContainerState extends State<ProjectContainer> {
+  bool isHovered = false;
   double scaleFactor = 1.0;
   @override
   Widget build(BuildContext context) {
+    final hoveredTransform = Matrix4.identity()..scale(1.02);
+    final transform = isHovered? hoveredTransform : Matrix4.identity();
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
     return MouseRegion(
       onEnter: (_) {
         setState(() {
-          scaleFactor = 1.1;
+          isHovered = true;
         });
       },
       onExit: (_) {
         setState(() {
-          scaleFactor = 1.0;
+          isHovered = false;
         });
       },
       child: Transform.scale(
-          scale: scaleFactor,
+        scale: scaleFactor,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 500),
+          transform: transform,
           child: ClipRRect(
             borderRadius: BorderRadius.circular(20),
-            child: screenWidth < 700 ?  Image.asset(
-              widget.image,
-              width:  screenWidth * 0.7 ,
-              height:  screenWidth * 0.7 ,
-            ) : Image.asset(
-              widget.image,
-              width: screenWidth < 1100? screenWidth * 0.45 : 500,
-              height: screenWidth < 1100? screenWidth * 0.45 : 500,
-            ),
-          )),
+            child: screenWidth < 700
+                ? Image.asset(
+                    widget.image,
+                    width: screenWidth * 0.7,
+                    height: screenWidth * 0.7,
+                  )
+                : Image.asset(
+                    widget.image,
+                    width: screenWidth < 1100 ? screenWidth * 0.45 : 500,
+                    height: screenWidth < 1100 ? screenWidth * 0.45 : 500,
+                  ),
+          ),
+        ),
+      ),
     );
   }
 }
